9824f54fdae1ab108f64369ee73b433eba7c839b
TChanel <tchanel.hammond@gmail.com>
2019-12-21 19:51:48 -0500

    Merge branch 'project_3' of https://github.com/aeraiba/CS371 into project_3
    # Conflicts:
    #	CS371_fall2019-master/Project 3/src/Partition.java
    #	CS371_fall2019-master/src/LinkedList.java
    #	CS371_fall2019-master/src/MapReduce.java

--- CS371_fall2019-master/src/Partition.java
+++ CS371_fall2019-master/src/Partition.java
@@ -4,15 +4,35 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-class Partition {
+class Partition <KV>{
 
-        int index;
-        Object key;
-        int value;
+    private final int size;
+    private final LinkedList<KV> list;
+    int index;
+    Object key;
+    int value;
+    KV next;
+    KV head;
 
-        public Partition(int index, Object key, int value) {
-            this.index = index;
-            this.key = key;
-            this.value = value;
+    public Partition(LinkedList <KV> list, int size) {
+        this.list = list;
+        this.size = size;
+
+    }
+
+    public Partition<KV> createPar(Object list, int size) {
+        if (list == null)
+            throw new NullPointerException( "File must not be null");
+        if(!(size >0)){
+            throw new IllegalArgumentException("Size must be greater than 0");
         }
+
+        return new Partition((LinkedList) list, size);
+    }
+
+    public void add(Object key, int value){
+        KV k1 = new KV();
+
+
+    }
     }
